dc9a836b1f434f1d25ad803e379f03c761950f8c
zkwap <564880775@qq.com>
2015-10-13 22:11:19 +0800

    dev test

--- Public/home/js/app.js
+++ Public/home/js/app.js
@@ -3,7 +3,8 @@ $('#page-content-wrapper').css('min-height', $('#sidebar-wrapper').height());
 
 var app = angular.module('indexApp',['ngRoute','ngAnimate']);
 app.config(['$routeProvider', '$locationProvider',function($routeProvider, $locationProvider) {
-		$routeProvider.when('/', {
+	
+		$routeProvider.when('/',  {
 			templateUrl: '/public/home/tmpl/tpl-list.html',
 			controller: 'ArticleListCtrl'
 		}).when('/detail/:id', {
